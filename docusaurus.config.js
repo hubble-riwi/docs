913e7a4258911b77c3e3b8ebf3a322f86c7fb69b
BrahianMS <johnbra696@gmail.com>
2025-09-08 21:17:06 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/hubble-riwi',
             label: 'GitHub',
             position: 'right',
           },
